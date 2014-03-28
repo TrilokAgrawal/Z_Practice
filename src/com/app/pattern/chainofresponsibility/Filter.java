c82421eef4f9b1cdfd278ccca89b82ccb7286e60
TrilokAgrawal <agarwal.trilok@gmail.com>
2014-03-28 10:08:07 +0530

    second commit (creation of one more branch before merging into master

--- src/com/app/pattern/chainofresponsibility/Filter.java
+++ src/com/app/pattern/chainofresponsibility/Filter.java
@@ -3,5 +3,7 @@ package com.app.pattern.chainofresponsibility;
 public abstract class Filter {
 	
 	//commit in branch test
+	
+	//committed in branch 1.0.0 but not merged into master, and go for another branch.
 
 }
